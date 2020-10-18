1c4a395043af37c3bf81d50cfc411c86e26f1cdd
Cordova, Jireh <archonist252@gmail.com>
2020-10-19 01:28:20 +0800

    Add chatroom view and code behind.

---
+++ Chat App/View/ChatRoom/ChatRoomViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRoomViewController.swift
+//  Chat App
+//
+//  Created by Cordova, Jireh on 10/19/20.
+//  Copyright © 2020 Cordova, Jireh. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
