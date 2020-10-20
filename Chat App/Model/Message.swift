2495bb3fdb53416c2ebbfce7f6f1364a0a96403c
Cordova, Jireh <archonist252@gmail.com>
2020-10-20 14:47:32 +0800

    Update messages model and chatroomVC.

--- Chat App/Model/Message.swift
+++ Chat App/Model/Message.swift
@@ -6,61 +6,72 @@
 //  Copyright © 2020 Cordova, Jireh. All rights reserved.
 //
 
-import Foundation
 import Firebase
 import MessageKit
-import UIKit
+import FirebaseFirestore
+import Foundation
 
-struct Message
+struct Message: MessageType
 {
-    var id: String
-    var date: String
-    var message: String
-    var senderName: String
+    let id: String?
+    let content: String
+    let sendDate: Date
+    let msgSender: Sender
     
-    var Dictionary: [String: Any]
-    {
-        return [
-            "id": id,
-            "date": date,
-            "message": message,
-            "senderName": senderName]
-    }
-}
-
-extension Message
-{
-    init?(dictionary: [String: Any])
-    {
-        guard let id = dictionary["id"] as? String,
-              let date = dictionary["date"] as? String,
-              let message = dictionary["message"] as? String,
-              let senderName = dictionary["senderName"] as? String
-        else {return nil}
-        
-        self.init(id: id, date: date, message: message, senderName: senderName)
-    }
-}
-
-extension Message: MessageType
-{
-    var sentDate: Date
-    {
-        return Date()
-    }
-
     var sender: SenderType
     {
-        return Sender(id: senderName, displayName: senderName)
+        return Sender(id:  AppSettings.displayName, displayName: AppSettings.displayName)
     }
-
+    
     var messageId: String
     {
-        return id
+        return id ?? UUID().uuidString
     }
-
+    
+    var sentDate: Date
+    {
+        return sendDate
+    }
+    
     var kind: MessageKind
     {
-        return .text(message)
+        return .text(content)
     }
+
+    init?(document: QueryDocumentSnapshot)
+    {
+      let data = document.data()
+      
+      guard let sentDate = data["sendDate"] as? Timestamp else {return nil}
+      guard let senderName = data["msgSender"] as? String else {return nil}
+      guard let content = data["content"] as? String else {return nil}
+        
+      
+      id = document.documentID
+      self.sendDate = sentDate.dateValue()
+      msgSender = Sender(id: senderName, displayName: senderName)
+      self.content = content
+      print(content)
+      
+    }
+    
+    init(content: String)
+    {
+      self.msgSender = Sender(id: AppSettings.displayName, displayName: AppSettings.displayName)
+      self.content = content
+      self.id = nil
+      self.sendDate = Date()
+    }
+}
+
+extension Message: Comparable {
+  
+  static func == (lhs: Message, rhs: Message) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+  static func < (lhs: Message, rhs: Message) -> Bool {
+    return lhs.sentDate < rhs.sentDate
+  }
+  
 }
--- Chat App/View/ChatRoom/ChatRoomViewController.swift
+++ Chat App/View/ChatRoom/ChatRoomViewController.swift
@@ -14,113 +14,86 @@ import InputBarAccessoryView
 //final class ChatRoomViewController: UIViewController {}
 final class ChatRoomViewController: MessagesViewController
 {
-    private var docReference: DocumentReference?
-    var messagesThread: [Message] = []
+    private let currentUser = AppSettings.displayName
+    private let db = Firestore.firestore()
+    private var reference: CollectionReference?
     
-    override func viewDidLoad() {
+    private var messageListener: ListenerRegistration?
+    private var messagesThread: [Message] = []
+    
+    deinit
+    {
+      messageListener?.remove()
+    }
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        self.title = "Chat"
-
-        navigationItem.largeTitleDisplayMode = .never
+        reference = db.collection(["Message Thread","MessageThread", "Messages"].joined(separator: "/"))
+        
+        messageListener = reference?.addSnapshotListener
+        { querySnapshot, error in
+          guard let snapshot = querySnapshot else
+          {
+            print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
+            return
+          }
+          
+          snapshot.documentChanges.forEach
+          { change in
+            self.handleDocumentChange(change)
+          }
+        }
+                
+        self.title = "Chat App"
+        navigationItem.setHidesBackButton(true, animated: false)
+        
         maintainPositionOnKeyboardFrameChanged = true
-        messageInputBar.inputTextView.tintColor = .lightGray
+        messageInputBar.inputTextView.tintColor = .gray
         messageInputBar.sendButton.setTitleColor(.green, for: .normal)
         
         messageInputBar.delegate = self as InputBarAccessoryViewDelegate
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
-        
-        loadChatRoom()
+        //loadChatRoom()
     }
-    
-    //MARK: - Class Methods
-    
-    func loadChatRoom()
-    {
-        let db = Firestore.firestore()
-        db.collection("Message Thread").getDocuments() { (messages, err) in
-            if let err = err
-            {
-                print ("Error getting documents: \(err)")
-                return
-            }
-            else
-            {
-                guard let messageCount = messages?.documents.count else
-                {
-                    return
-                }
-                
-                if messageCount == 0
-                {
-                    self.createChatRoom()
-                    
-                }
-                else if messageCount >= 1
-                {
-                    for message in messages!.documents
-                    {
-                        self.docReference = message.reference
-                        message.reference.collection("Message Thread")
-                            .order(by: "Date Created", descending: false)
-                            .addSnapshotListener(includeMetadataChanges: true, listener:
-                            { (threadQuery, error) in
-                                
-                                if let error = error
-                                {
-                                    NSLog("Error: \(error)")
-                                    return
-                                }
-                                else
-                                {
-                                    self.messagesThread.removeAll()
-                                    for message in threadQuery!.documents
-                                    {
-                                            let msg = Message(dictionary: message.data())
-                                            self.messagesThread.append(msg!)
-                                        print("Data: \(msg?.message ?? "No message found")")
-                                    }
-                                    
-                                    self.messagesCollectionView.reloadData()
-                                    self.messagesCollectionView.scrollToBottom(animated: true)
-                                }
-                            })
-                        return
-                    }
-                    self.createChatRoom()
-                }
-                else
-                {
-                    NSLog("Load Chats Error")
-                }
-            }
-        }
-    }
-    
-    func createChatRoom()
-    {
-        let db = Firestore.firestore()
-        db.collection("Message Thread2").addDocument(data: ["": ""])
-        { (error) in
-            
-            if let error = error
-            {
-                print("Unable to create chat! \(error)")
-                return
-            }
-            else
-            {
-                self.loadChatRoom()
-            }
-        }
-    }
-    
+ 
     //MARK: - Class Methods R.
-    private func inserNewMessage (_ message: Message)
+    
+    // saves message as new document in the message thread
+    private func save (_ message: Message)
     {
+        let messageData: [String: Any] =
+        [
+            "id" : message.messageId,
+            "sendDate" : message.sendDate,
+            "content" : message.content,
+            "msgSender" : message.msgSender.displayName
+        ]
+        
+        reference?.addDocument(data: messageData)
+        { error in
+          if let e = error
+          {
+            print("Error sending message: \(e.localizedDescription)")
+            return
+          }
+          
+          self.messagesCollectionView.scrollToBottom()
+        }
+    }
+    
+    private func insertNewMessage (_ message: Message)
+    {
+        guard !messagesThread.contains(message) else
+        {
+          return
+        }
+        
         messagesThread.append(message)
+        messagesThread.sort()
         messagesCollectionView.reloadData()
         
         DispatchQueue.main.async
@@ -129,39 +102,32 @@ final class ChatRoomViewController: MessagesViewController
         }
     }
     
-    // saves message as new document in the message thread
-    private func save (_ message: Message)
+    private func handleDocumentChange(_ change: DocumentChange)
     {
-        let data: [String: Any] =
-        [
-            "Message": "Test ",
-            "Sender": "SCP-063"
-        ]
-        
-        docReference?.collection("Message Thread").addDocument(data: data, completion:
-        { (error) in
-            if let error = error
-            {
-                print("Error Sending message: \(error)")
-                return
-            }
-            
-            self.messagesCollectionView.scrollToBottom()
-        })
+        guard let message = Message(document: change.document) else
+        {
+          return
+        }
+
+        switch change.type
+        {
+            case .added:
+                insertNewMessage(message)
+            default:
+                break
+        }
     }
 }
 
-//MARK: - MessageInputBarDelegate
-extension ChatRoomViewController: MessageInputBarDelegate
+//MARK: - InputBar Delegate
+extension ChatRoomViewController: InputBarAccessoryViewDelegate
 {
-    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
-      let message = Message(id: "", date: "", message: text, senderName: "test")
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String)
+    {
+        let message = Message(content: text)
 
-      save(message)
-      inserNewMessage(message)
-      inputBar.inputTextView.text = ""
-      messagesCollectionView.reloadData()
-      messagesCollectionView.scrollToBottom(animated: true)
+        save(message)
+        inputBar.inputTextView.text = ""
     }
 }
 
@@ -170,7 +136,8 @@ extension ChatRoomViewController: MessagesDataSource
 {
     func currentSender() -> SenderType
     {
-        return Sender(id: "someID", displayName: "TestUser")
+        NSLog(currentUser!)
+        return Sender(id: currentUser!, displayName: currentUser!)
     }
 
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType
@@ -184,30 +151,63 @@ extension ChatRoomViewController: MessagesDataSource
             print("No messages to display")
             return 0
         } else {
+            print(messagesThread.count)
             return messagesThread.count
         }
     }
 }
 
-//MARK: - MessagesLayoutDelegate
+//MARK: Messages Layout
 extension ChatRoomViewController: MessagesLayoutDelegate
 {
-    
+  func avatarSize(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGSize
+  {
+    return .zero
+  }
+  
+  func footerViewSize(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGSize
+  {
+    return CGSize(width: 0, height: 8)
+  }
+  
+  func heightForLocation(message: MessageType, at indexPath: IndexPath, with maxWidth: CGFloat, in messagesCollectionView: MessagesCollectionView) -> CGFloat
+  {
+    return 0
+  }
+  
 }
 
 //MARK: - MessageDisplayDelegate
 extension ChatRoomViewController: MessagesDisplayDelegate
 {
-    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor
+    {
         return isFromCurrentSender(message: message) ? UIColor.green : UIColor.green
     }
     
-    func shouldDisplayHeader(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> Bool {
+    func shouldDisplayHeader(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> Bool
+    {
       return false
     }
     
-    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle
+    {
       let corner: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
         return .bubbleTail(corner, .pointedEdge)
+    }
+    
+    func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor
+    {
+        return .white
+    }
+}
+
+extension Date
+{
+    func toString( dateFormat format  : String ) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
     }
 }
