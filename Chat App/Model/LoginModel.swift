714884daa0359ecec5abadaf35b95d16a5e182d4
Cordova, Jireh <archonist252@gmail.com>
2020-10-23 06:23:07 +0800

    Update models.

---
+++ Chat App/Model/LoginModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginModel.swift
+//  Chat App
+//
+//  Created by Cordova, Jireh on 10/23/20.
+//  Copyright © 2020 Cordova, Jireh. All rights reserved.
+//
+
+import Foundation
+
+struct LoginModel
+{
+    let textFieldEmptyWarningMessage = "Please enter a value."
+    let duplicateUserWarning = "Username is taken."
+    let invalidInputWarning = "Value is incorrect."
+    let signupButtonLabel = "Sign Up"
+    let loginButtonLabel = "Login"
+}
---
+++ Chat App/Model/SignupModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignupModel.swift
+//  Chat App
+//
+//  Created by Cordova, Jireh on 10/22/20.
+//  Copyright © 2020 Cordova, Jireh. All rights reserved.
+//
+
+import Foundation
+
+struct SignupModel
+{
+    let textFieldEmptyWarningMessage = "Please enter a value."
+    let duplicateUserWarning = "Username is taken."
+    let invalidInputWarning = "Value is incorrect."
+    let signupButtonLabel = "Sign Up"
+    let loginButtonLabel = "Login"
+}
