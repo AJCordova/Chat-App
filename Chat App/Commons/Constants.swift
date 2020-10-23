e13925d7f0065c2e0d2cfb5d6bd971a869aeffb3
Cordova, Jireh <archonist252@gmail.com>
2020-10-23 13:20:38 +0800

    Add constant values for reused strings.

--- Chat App/Commons/Constants.swift
+++ Chat App/Commons/Constants.swift
@@ -14,8 +14,7 @@ struct Constants
     static let userAgreementText: String = "By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. Others will be able to find you by searching for your email address or phone number when provided."
     static let signupButtonLabel: String = "Sign up"
     static let loginButtonLabel: String = "Login"
-    static let usernameWarningLabel: String = "Value is incorrect"
-    static let passwordWarningLabel: String = "Value is incorrect"
+    static let invalidInputWarning: String = "Value is incorrect"
     static let NavigationBarTitle: String = "Chat app"
     static let duplicateUserWarningLabel: String = "Username is taken"
     
