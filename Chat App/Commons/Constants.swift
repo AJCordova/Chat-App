f1f39357c078c8bf45bf30d92ae3f4cd98b787d0
Cordova, Jireh <archonist252@gmail.com>
2020-10-23 19:18:17 +0800

    Update Constants.swift

--- Chat App/Commons/Constants.swift
+++ Chat App/Commons/Constants.swift
@@ -12,6 +12,7 @@ import UIKit
 struct Constants
 {
     //MARK: - UI Labels and Texts
+    static let navigationTitle: String = "Chat app"
     static let userAgreementText: String = "By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. Others will be able to find you by searching for your email address or phone number when provided."
     static let signupButtonLabel: String = "Sign up"
     static let loginButtonLabel: String = "Login"
@@ -21,4 +22,5 @@ struct Constants
     
     //MARK: - Color
     static let messageBubbleColor = UIColor(red: 158, green: 223, blue: 71)
+    static let messageInputBackground = UIColor(red: 245, green: 245, blue: 245)
 }
