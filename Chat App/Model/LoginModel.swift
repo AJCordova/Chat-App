5a3f99fd1dc22932e235876012d5383ead44021f
Cordova, Jireh <archonist252@gmail.com>
2020-10-23 13:21:11 +0800

    Update models to include database path refs.

--- Chat App/Model/LoginModel.swift
+++ Chat App/Model/LoginModel.swift
@@ -10,9 +10,6 @@ import Foundation
 
 struct LoginModel
 {
-    let textFieldEmptyWarningMessage = "Please enter a value."
-    let duplicateUserWarning = "Username is taken."
-    let invalidInputWarning = "Value is incorrect."
-    let signupButtonLabel = "Sign Up"
-    let loginButtonLabel = "Login"
+    let CollectionReferrence = "Users"
+    let FieldReferrence = "username"
 }
