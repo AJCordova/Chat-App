7c8e4fddf3173d10a6e88153e5486dc55dbf0da8
AJCordova <archonist252@gmail.com>
2020-12-07 13:08:05 +0800

    Fix ViewModel classes styling.

--- Chat App/ViewModel/SignUpViewModel.swift
+++ Chat App/ViewModel/SignUpViewModel.swift
@@ -9,15 +9,13 @@
 import Foundation
 import FirebaseFirestore
 
-protocol SignUpViewModelDelegate
-{
+protocol SignUpViewModelDelegate {
     func processUserCredentials(from username: String?, password: String?)
     var usernameWarningMessage: String { get }
     var passwordWarningMessage: String { get }
 }
 
-class SignUpViewModel: SignUpViewModelDelegate
-{
+class SignUpViewModel: SignUpViewModelDelegate {
     let task = DispatchGroup()
     var usernameWarningMessage: String = ""
     var passwordWarningMessage: String = ""
@@ -31,8 +29,7 @@ class SignUpViewModel: SignUpViewModelDelegate
     private var didEncounterError = false
     private var model = SignupModel()
     
-    init()
-    {
+    init() {
         self.collectionName = model.CollectionReferrence
         self.referrence = db.collection(collectionName)
     }
@@ -45,27 +42,22 @@ class SignUpViewModel: SignUpViewModelDelegate
         - Parameter password: submitted password
         - Returns: bool
      */
-    func processUserCredentials(from username: String?, password: String?)
-    {
+    func processUserCredentials(from username: String?, password: String?) {
         guard let userText = username else {return}
         guard let passwordText = password else {return}
 
-        if userText.isEmpty && passwordText.isEmpty
-        {
+        if userText.isEmpty && passwordText.isEmpty {
             NSLog("Input Empty")
             usernameWarningMessage = Constants.invalidInputWarning
             passwordWarningMessage = Constants.invalidInputWarning
             self.delegate?.showWarnings()
             return
         }
-        else
-        {
-            if (validateUserInput(username: userText, password: passwordText))
-            {
+        else {
+            if (validateUserInput(username: userText, password: passwordText)) {
                 isUsernameRegistered(username: userText, password: passwordText)
             }
-            else
-            {
+            else {
                 usernameWarningMessage = Constants.invalidInputWarning
                 passwordWarningMessage = Constants.invalidInputWarning
                 self.delegate?.showWarnings()
@@ -80,15 +72,12 @@ class SignUpViewModel: SignUpViewModelDelegate
      - Parameter password: submitted password
      - Returns: bool
      */
-    func validateUserInput(username: String, password: String) -> Bool
-    {
-        if username.count < 8 || username.count > 16
-        {
+    func validateUserInput(username: String, password: String) -> Bool {
+        if username.count < 8 || username.count > 16 {
             return false
         }
         
-        if password.count < 8 || password.count > 16
-        {
+        if password.count < 8 || password.count > 16 {
             return false;
         }
         
@@ -101,36 +90,29 @@ class SignUpViewModel: SignUpViewModelDelegate
      - Parameter password: submitted password
      - Returns: bool
      */
-    private func registerUser (username: String, password: String)
-    {
+    private func registerUser (username: String, password: String) {
         var docReferrence: DocumentReference? = nil
         let hash = password.base64Encoded()
         task.enter()
         
-        if self.didEncounterError
-        {
+        if self.didEncounterError {
             NSLog("Sign up failed")
             task.leave()
         }
         
-        if (self.hasFoundDuplicates)
-        {
+        if (self.hasFoundDuplicates) {
             NSLog("Sign up failed")
             task.leave()
         }
-        else
-        {
-            docReferrence = referrence!.addDocument(data: ["username": username,"password": hash!])
-            { error in
-                if let error = error
-                {
+        else {
+            docReferrence = referrence!.addDocument(data: ["username": username,"password": hash!]) { error in
+                if let error = error {
                     NSLog("Error adding user: \(error)")
                     self.usernameWarningMessage = Constants.invalidInputWarning
                     self.passwordWarningMessage = Constants.invalidInputWarning
                     self.didEncounterError = true
                 }
-                else
-                {
+                else {
                     NSLog("User added. Reference: \(docReferrence?.documentID ?? "")")
                     AppSettings.userID = docReferrence?.documentID
                     AppSettings.displayName = username
@@ -140,25 +122,20 @@ class SignUpViewModel: SignUpViewModelDelegate
             }
         }
         
-        task.notify(queue: .main)
-        {
-            if self.didEncounterError
-            {
+        task.notify(queue: .main) {
+            if self.didEncounterError {
                 NSLog("Firebase error")
                 self.delegate?.isSignupSuccessful(result: false) // better have a generic error message. but sticking to specs for now.
             }
-            else if self.hasFoundDuplicates
-            {
+            else if self.hasFoundDuplicates {
                 NSLog("Found user duplicates")
                 self.delegate?.showWarnings()
             }
-            else if self.isSignupSuccess
-            {
+            else if self.isSignupSuccess {
                 NSLog("Sign up Success")
                 self.delegate?.isSignupSuccessful(result: true)
             }
-            else
-            {
+            else {
                 NSLog("Sign up failed")
                 self.delegate?.isSignupSuccessful(result: false)
             }
@@ -171,22 +148,17 @@ class SignUpViewModel: SignUpViewModelDelegate
      - Parameter password: submitted password
      - Returns: bool
      */
-    private func isUsernameRegistered (username: String, password: String)
-    {
+    private func isUsernameRegistered (username: String, password: String) {
         task.enter()
-        referrence?.whereField(model.FieldReferrence, isEqualTo: username).getDocuments()
-        { (snapshot, err) in
-            if let err = err
-            {
+        referrence?.whereField(model.FieldReferrence, isEqualTo: username).getDocuments() { (snapshot, err) in
+            if let err = err {
                 NSLog("isUsernameRegistered(): Error getting document: \(err)")
                 self.didEncounterError = true
             }
-            else if (snapshot!.isEmpty)
-            {
+            else if (snapshot!.isEmpty) {
                 NSLog("isUsernameRegistered(): No duplicates found")
             }
-            else
-            {
+            else {
                 NSLog("isUsernameRegistered(): duplicates found")
                 self.usernameWarningMessage = Constants.duplicateUserWarningLabel
                 self.passwordWarningMessage = ""
@@ -195,8 +167,7 @@ class SignUpViewModel: SignUpViewModelDelegate
             self.task.leave()
         }
         
-        task.notify(queue: .global())
-        {
+        task.notify(queue: .global()) {
             NSLog("isUsernameRegistered(): Proceed to registerUser()")
             self.registerUser(username: username, password: password)
         }
