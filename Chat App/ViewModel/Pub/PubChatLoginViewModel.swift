ec5bcde0544bd67cd08b6c0962fdc79ba2be4261
AJCordova <archonist252@gmail.com>
2020-12-11 17:15:14 +0800

    Add PubChatLoginViewModel

---
+++ Chat App/ViewModel/Pub/PubChatLoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PubChatLoginViewModel.swift
+//  Chat App
+//
+//  Created by Amiel Jireh Cordova on 12/11/20.
+//  Copyright © 2020 Cordova, Jireh. All rights reserved.
+//
+
+import Foundation
+
+class PubChatLoginViewModel {
+    
+}
