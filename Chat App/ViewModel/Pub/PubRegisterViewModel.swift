55a70dbc5fcb6a0b62f77293e733eb666b5148cf
Jireh Cordova <amieljireh_cordova@ligph.com>
2020-12-29 14:15:08 +0800

    Add a method call to register new users.
    - Add observables for alert and loading views.

--- Chat App/ViewModel/Pub/PubRegisterViewModel.swift
+++ Chat App/ViewModel/Pub/PubRegisterViewModel.swift
@@ -11,13 +11,18 @@ import RxCocoa
 import RxSwift
 
 class PubRegisterViewModel {
+    var shouldShowAlert: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    var shouldShowLoading: BehaviorRelay<Bool> = BehaviorRelay(value: false)
     var isUsernameAvailable: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-    var doPasswordsMatch: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    var shouldProceedtoServer: BehaviorRelay<Bool> = BehaviorRelay(value: false)
     
+    var alertTitle = "Oh no"
+    var alertMessage = "This service is currently unavailable. Please try again later."
+    var alertActionLabel = "Ok"
     var passwordInput = ""
     var submittedUsername = ""
+    var userManager: UserManagementProtocol
     
-    private var userManager: UserManagementProtocol
     private let disposeBag = DisposeBag()
     
     init() {
@@ -28,7 +33,7 @@ class PubRegisterViewModel {
     /**
      Verifies that user input meets character limit requirements.
      - Parameter userInput: submitted user input.
-     - Returns a Bool value that indicates if user input was valid. 
+     - Returns a Bool value that indicates if user input was valid.
      */
     func verifyUserInput(userInput: String) -> Bool {
         if userInput.count >= 8 && userInput.count <= 16 {
@@ -50,6 +55,7 @@ class PubRegisterViewModel {
      Registers a new Pub user.
      */
     func registerUser() {
+        shouldShowLoading.accept(true)
         let encodedPassword = hashPassword()
         userManager.registerNewUser(username: submittedUsername, password: encodedPassword)
     }
@@ -57,7 +63,7 @@ class PubRegisterViewModel {
     /**
      Verifies that the inputted username is valid.
      - Parameter userInput: submitted user input.
-     - Returns a password as a base64 string.
+     - Returns a base64 encoded string.
      */
     func hashPassword() -> String {
         return passwordInput.base64Encoded()!
@@ -78,9 +84,9 @@ class PubRegisterViewModel {
     }
     
     /**
-     Sets up observable subjects for pub register viewcontroller.
+     Setup observers for user manager subjects.
      */
-    func setUpObservers() {
+    private func setUpObservers() {
         userManager.isUsernameAvailable
             .asObservable()
             .skip(1)
@@ -91,6 +97,32 @@ class PubRegisterViewModel {
                     self.isUsernameAvailable.accept(true)
                 } else {
                     self.isUsernameAvailable.accept(false)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        userManager.hasExitedPrematurely
+            .asObservable()
+            .skip(1)
+            .subscribe(onNext: { [weak self] hasExitedPrematurely in
+                guard let self = `self`,
+                      let hasExitedPrematurely = hasExitedPrematurely.rawValue as? Bool else { return }
+                if hasExitedPrematurely {
+                    self.shouldShowLoading.accept(false)
+                    self.shouldShowAlert.accept(true)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        userManager.isRegisterSuccessful
+            .asObservable()
+            .skip(1)
+            .subscribe(onNext: { [weak self] isSuccessful in
+                guard let self = `self`,
+                      let isSuccessful = isSuccessful.rawValue as? Bool else { return }
+                self.shouldShowLoading.accept(false)
+                if isSuccessful {
+                    self.shouldProceedtoServer.accept(true)
                 }
             })
             .disposed(by: disposeBag)
