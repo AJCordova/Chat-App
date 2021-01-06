313f2caf424f6b906e0f5d0ec808feeade2b1f6e
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-06 15:29:26 +0800

    Revert "Fixing test target dependencies."
    This reverts commit db99b8534f6326f2cd7f1729336b022534185d82.

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "Chat AppTests",
-            dependencies: []),
+            dependencies: ["Chat-App"]),
     ]
 )
