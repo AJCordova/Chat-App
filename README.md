749dcd4ee4d70f13f3a18f5b4012a24e52d3f467
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-06 10:42:54 +0800

    Initialize package manager.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat-App
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Chat_AppTests
+
+var tests = [XCTestCaseEntry]()
+tests += Chat_AppTests.allTests()
+XCTMain(tests)
