236dcbd96e27347392f8fa036f65733fd2a93ef5
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-06 12:36:20 +0800

    Fix target names.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Chat-App",
+            name: "Chat App",
             targets: ["Chat App"]),
     ],
     dependencies: [
