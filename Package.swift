c88a42e78501f0117af4e5c77d3aa8c27c761d35
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-07 12:00:17 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Chat App",
+    name: "Chat-App",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
