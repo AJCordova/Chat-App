79abb34744e943eaeced5b77fd15eecb85112c7f
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-07 12:37:21 +0800

    Update variables.

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Chat-App",
+    name: "Chat App",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Chat-App",
+            name: "Chat App",
             targets: ["Chat App"]),
     ],
     dependencies: [
