0f39a51030fd542b3f158effa2b5e79dbaf99133
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-01-13 11:11:40 +0800

    Update registerviewmodel.
    - Conform to the protocol pattern.
    - Update observables.

--- Chat App/ViewModel/Pub/PubRegisterViewModel.swift
+++ Chat App/ViewModel/Pub/PubRegisterViewModel.swift
@@ -12,8 +12,9 @@ import RxSwift
 
 protocol RegisterViewModelInputs {
     func verifyUsernameAvailability(userInput: String)
-    func verifyUserInput(userInput: String) -> Bool
-    func verifyPasswordMatch(userInput: String!, comparable: String!) -> Bool
+    func usernameChanged(userInput: String!)
+    func passwordChanged(userInput: String!)
+    func verifyPasswordMatch(userInput: String, comparable: String)
     func registerUser(submittedUsername: String, password: String)
 }
 
@@ -25,6 +26,10 @@ protocol RegisterViewModelOutputs {
     var shouldShowLoading: PublishSubject<Bool> { get }
     var isUsernameAvailable: PublishSubject<Bool> { get }
     var shouldProceedtoServer: PublishSubject<Bool> { get }
+    var doPasswordsMatch: PublishSubject<Bool> { get }
+    var isPasswordValid: PublishSubject<Bool> { get }
+    var isUsernameValid: PublishSubject<Bool> { get }
+    var shouldEnableRegisterButton: BehaviorRelay<Bool> { get }
 }
 
 protocol RegisterViewModelTypes {
@@ -37,8 +42,13 @@ class PubRegisterViewModel: RegisterViewModelInputs, RegisterViewModelOutputs, R
     var shouldShowLoading = PublishSubject<Bool>()
     var isUsernameAvailable = PublishSubject<Bool>()
     var shouldProceedtoServer = PublishSubject<Bool>()
+    var isPasswordValid = PublishSubject<Bool>()
+    var isUsernameValid = PublishSubject<Bool>()
+    var doPasswordsMatch = PublishSubject<Bool>()
+    var shouldEnableRegisterButton = BehaviorRelay<Bool>(value: false)
     var inputs: RegisterViewModelInputs { return self }
     var outputs: RegisterViewModelOutputs { return self }
+    
     
     var alertTitle: String = ""
     var alertMessage: String = ""
@@ -50,6 +60,14 @@ class PubRegisterViewModel: RegisterViewModelInputs, RegisterViewModelOutputs, R
     init() {
         self.userManager = UserManagementService()
         setUpObservers()
+        
+        let everythingValid = Observable
+            .combineLatest(isUsernameValid, isPasswordValid, doPasswordsMatch)
+            .map { $0 && $1 && $2 }
+        
+        everythingValid
+            .bind(to: shouldEnableRegisterButton)
+            .disposed(by: disposeBag)
     }
     
     /**
@@ -57,7 +75,7 @@ class PubRegisterViewModel: RegisterViewModelInputs, RegisterViewModelOutputs, R
      - Parameter userInput: submitted user input.
      - Returns a Bool value that indicates if user input was valid.
      */
-    func verifyUserInput(userInput: String) -> Bool {
+    func isInputValid(userInput: String) -> Bool {
         userInput.count >= 8 && userInput.count <= 16
     }
     
@@ -93,15 +111,31 @@ class PubRegisterViewModel: RegisterViewModelInputs, RegisterViewModelOutputs, R
      Verifies that the password and confirm password fields contain the same values.
      - Parameter userInput: submitted user input.
      - Parameter comparable: string baseline to evaluate user input.
-     - Returns a Bool value that indicates that values match.
      */
-    func verifyPasswordMatch(userInput: String!, comparable: String!) -> Bool {
-        guard let userInput = userInput,
-              let comparable = comparable else { return false }
+    func verifyPasswordMatch(userInput: String, comparable: String) {
         if userInput.elementsEqual(comparable) {
-            return true
+            doPasswordsMatch.onNext(true)
         } else {
-            return false
+            doPasswordsMatch.onNext(false)
+        }
+    }
+    
+    func usernameChanged(userInput: String!) {
+        guard let userInput = userInput else { return }
+        if isInputValid(userInput: userInput) {
+            isUsernameValid.onNext(true)
+            verifyUsernameAvailability(userInput: userInput)
+        } else {
+            isUsernameValid.onNext(false)
+        }
+    }
+    
+    func passwordChanged(userInput: String!) {
+        guard let userInput = userInput else { return }
+        if isInputValid(userInput: userInput) {
+            isPasswordValid.onNext(true)
+        } else {
+            isPasswordValid.onNext(false)
         }
     }
     
